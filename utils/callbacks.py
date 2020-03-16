a9696330e134bdd71904915743cf098fd0673acf
mayankbhaskar007 <mayankbhaskar007@gmail.com>
2020-03-16 10:31:40 +0530

    Added a function for inception module

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+from keras.layers import Conv2D
+
+
 class CustomCallback(Callback):
 
     def __init__(self, run_folder, print_every_n_batches, initial_epoch, vae):
@@ -38,3 +41,12 @@ def step_decay_schedule(initial_lr, decay_factor=0.5, step_size=1):
         return new_lr
 
     return LearningRateScheduler(schedule)
+
+def inception_block():
+    '''
+    Inception block from the paper Network in Network: https://arxiv.org/abs/1312.4400
+
+    :return:
+    '''
+    #Firdt layer defining
+    #input = Conv2D(filters)
